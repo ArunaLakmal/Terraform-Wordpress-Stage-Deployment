4d1454974ac37d977bc7a5e71f08e0b84f440d69
Aruna Lakmal <arunarandunu@gmail.com>
2019-11-21 06:02:46 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -496,7 +496,7 @@ resource "aws_autoscaling_group" "wp_asg" {
 
   launch_configuration = "${aws_launch_configuration.wp_lc.name}"
 
-  tag = {
+  tags = {
     key                 = "Name"
     value               = "wp_asg-instance"
     propagate_at_launch = true
