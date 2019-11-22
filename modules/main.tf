4e650ca6c180595451dfbb28e43f0cf959c99f68
Aruna Lakmal <arunarandunu@gmail.com>
2019-11-22 11:47:28 +0530

    backend config

--- modules/main.tf
+++ modules/main.tf
@@ -2,7 +2,10 @@ provider "aws" {
   region  = "${var.aws_region}"
   profile = "${var.aws_profile}"
 }
-
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+}
 #---- IAM ----
 
 #S3_access
